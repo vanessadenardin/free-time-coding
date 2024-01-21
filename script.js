f7b4c726c5e64bc63b4b57d92db22d3c223020cd
Vanessa Denardin <vanessa.denardin@gmail.com>
2024-01-21 18:12:53 +1100

    updated goStore function

--- script.js
+++ script.js
@@ -49,14 +49,7 @@ function goTown() {
 }
 
 function goStore() {
-    // console.log("Going to store.");
-    // button1.innerText = "Buy 10 health (10 gold)";
-    // button2.innerText = "Buy weapon (30 gold)";
-    // button3.innerText = "Go to town square"
-    // button1.onclick = buyHealth;
-    // button2.onclick = buyWeapon;
-    // button3.onclick = goTown;
-    // text.innerText = "You enter the store.";
+    update(locations[1]);
 }
 
 function goCave() {
