045f426387f49400514dbc42dd31233be6c5eacb
Vanessa Denardin <vanessa.denardin@gmail.com>
2024-01-21 22:10:38 +1100

    added innerText to buyHealth function

--- script.js
+++ script.js
@@ -28,19 +28,19 @@ const locations = [
         "button text": ["Go to store", "Go to cave", "Fight dragon"],
         "button functions": [goStore, goCave, fightDragon],
         text: "You are in the town square. You see a sign that says \"Store\"."
-},
-{
-    name: "store",
-    "button text": ["Buy 10 health (10 gold)", "Buy weapon (30 gold)", "Go to town square"],
-    "button functions": [buyHealth, buyWeapon,goTown],
-    text: "You enter the store."
-},
-{
-    name: "cave",
-    "button text": ["Fight slime", "Fight fanged beast", "Go to town square"],
-    "button functions": [fightSlime, fightBeast, goTown],
-    text: "You enter the cave. You see some monsters."
-}
+    },
+    {
+        name: "store",
+        "button text": ["Buy 10 health (10 gold)", "Buy weapon (30 gold)", "Go to town square"],
+        "button functions": [buyHealth, buyWeapon,goTown],
+        text: "You enter the store."
+    },
+    {
+        name: "cave",
+        "button text": ["Fight slime", "Fight fanged beast", "Go to town square"],
+        "button functions": [fightSlime, fightBeast, goTown],
+        text: "You enter the cave. You see some monsters."
+    }
 ];
 
 function goTown() {
@@ -85,7 +85,12 @@ button2.onclick = goCave;
 button3.onclick = fightDragon;
 
 function buyHealth() {
-    gold = gold - 10;
+    gold -= 10;
+    health += 10;
+    
+    goldText.innerText = gold;
+
+    healthText.innerText = health;
 }
 
 function buyWeapon() {
