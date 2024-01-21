f32f4a8d32084ecfc6a13528e418c059c1576932
Vanessa Denardin <vanessa.denardin@gmail.com>
2024-01-21 16:58:13 +1100

    added button 2 and 3 for function goStore

--- script.js
+++ script.js
@@ -25,6 +25,8 @@ const monsterHealthText = document.querySelector("#monsterHealth")
 function goStore() {
     // console.log("Going to store.");
     button1.innerText = "Buy 10 health (10 gold)";
+    button2.innerText = "Buy weapon (30 gold)";
+    button3.innerText = "Go to town square"
 }
 
 function goCave() {
