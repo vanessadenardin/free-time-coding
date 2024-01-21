cfa375258bb03a2859af37e003bb5185fa785fdf
Vanessa Denardin <vanessa.denardin@gmail.com>
2024-01-21 17:58:17 +1100

    updated button1 in update function

--- script.js
+++ script.js
@@ -82,7 +82,8 @@ function buyWeapon() {
 }
 
 function update(location){
-    button1.innerText = "Go to store";
+    // button1.innerText = "Go to store";
+    button1.innerText = location["button text"];
     button2.innerText = "Go to cave";
     button3.innerText = "Fight dragon";
     button1.onclick = goStore;
