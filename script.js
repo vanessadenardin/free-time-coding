134e03dd727f528d467a1449d2a9e64572ed93c5
Vanessa Denardin <vanessa.denardin@gmail.com>
2024-01-22 19:19:15 +1100

    added buyWeapon function logic

--- script.js
+++ script.js
@@ -22,6 +22,25 @@ const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth")
 
+const weapons = [
+    {
+        name: "stick" ,
+        power: 5 ,
+    },
+    {
+        name: "dagger" ,
+        power: 30 ,
+    },
+    {
+        name: "claw hammer" ,
+        power: 50 ,
+    },
+    {
+        name: "sword" ,
+        power: 100 ,
+    },
+];
+
 const locations = [
     {
         name: "town square",
@@ -96,7 +115,17 @@ function buyHealth() {
 }
 
 function buyWeapon() {
-
+    if (gold >= 30) {
+        gold -= 30;
+        currentWeapon++;
+        goldText.innerText = gold;
+        let newWeapon = weapons[currentWeapon].name;
+        text.innerText = "You now have a " + newWeapon + ".";
+        inventory.push(newWeapon);
+        text.innerText += " In your inventory you have: " + inventory;
+    } else {
+        text.innerText = "You do not have enough gold to buy a weapon."
+    }
 }
 
 function fightSlime() {
