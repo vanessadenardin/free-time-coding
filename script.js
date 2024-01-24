e40547e791ef65f6200f2508f7ae0c426976b290
Vanessa Denardin <vanessa.denardin@gmail.com>
2024-01-24 20:24:14 +1100

    updated goFight function

--- script.js
+++ script.js
@@ -166,7 +166,11 @@ function sellWeapon(){
 }
 
 function goFight(){
-    update(locations[3])
+    update(locations[3]);
+    monsterHealth = monsters[fighting].health;
+    monsterStats.style.display = "block";
+    monsterName.innerText = monsters[fighting].name;
+    monsterHealthText.innerText = monsters[fighting].health;
 }
 
 function fightSlime() {
