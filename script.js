97bce11e602d1e8425695c604d4afc9d68608d28
Vanessa Denardin <vanessa.denardin@gmail.com>
2024-01-28 18:41:03 +1100

    updated pick function

--- script.js
+++ script.js
@@ -293,7 +293,26 @@ function easterEgg(){
 }
 
 function pick(guess){
-  
+    const numbers = [];
+    while (numbers.length < 10) {
+        numbers.push(Math.floor(Math.random() * 11))
+    };
+    text.innerText = "You picked " + guess + ". Here are the random numbers:\n";
+    for (let i = 0; i < 10; i++) {
+        text.innerText += numbers[i] + "\n"
+        if (numbers.includes(guess)) {
+            text.innerText += "Right! You win 20 gold!";
+            gold += 20;
+            goldText.innerText = goldText;
+        } else {
+            text.innerText += "Wrong! You lose 10 health!";
+            health -= 10;
+            healthText.innerText = health;
+            if (health <= 0) {
+                lose()
+            }
+        }
+    }
 }
 
 function pickTwo(){
